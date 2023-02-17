2905485fca2caeeb7a4c4e61ddfe9aed1dfa9b6b
jaykarn singh <55475521+JaykarnSingh@users.noreply.github.com>
2023-02-18 03:14:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
+import {useState,useEffect} from 'react'
 function App() {
+  const [search,setSearch]=useState("");
+  const [apidata,setApidata]=useState();
+
+  useEffect(()=>{
+    const fetchData=async()=>{
+      const res= await  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=e63f307dc798d293247eb8b9d0c6acd8`)
+      const data=await res.json()
+    
+    if(res.ok){
+      setApidata(data)
+    }
+    }
+    
+    fetchData()
+  },[search])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Weather App</h1>
+      <input type='text' name="search" placeholder="Enter city" onChange={(e)=>setSearch(e.target.value)} value={search}/>
+      {apidata? (<div>
+         <p><span>Name : {apidata.name}</span></p>
+         <p><span>Max Temp : {apidata.main.temp_max}</span></p>
+         <p><span>Min Temp : {apidata.main.temp_min}</span></p>
+         <p><span>Country : {apidata.sys.country}</span></p>
+        </div>)
+        :
+        (<p>Result not found</p>)
+      }
     </div>
   );
 }
